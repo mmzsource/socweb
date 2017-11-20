3aa398fb900ca55afbb48b6728cb822ec6fd5d72
MMz <mmz.source@gmail.com>
2017-11-20 13:26:05 +0100

    Add new project with default lein template.
    This project is meant to improve my clojure webdev skills.
    I'll deploy a REST service and a simple web UI.
    Together they'll calculate and present the current standing
    in our penalty point competition.

---
+++ src/socweb/core.clj
@@ -0,0 +1,6 @@
+(ns socweb.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/socweb/core_test.clj
@@ -0,0 +1,7 @@
+(ns socweb.core-test
+  (:require [clojure.test :refer :all]
+            [socweb.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
